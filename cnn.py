0f73a8704422b8193e4d514734efb4a5bac1231a
raphael-baena <raphael.baena@gmail.com>
2018-04-10 14:44:14 +0200

    cnn.py changed convolutions give a tensor (N_time,Kuser,Nsubcarrier) but nn.linear can't be used

--- cnn.py
+++ cnn.py
@@ -43,16 +43,16 @@ class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(12, 16, kernel_size=5, padding=2),
-            nn.BatchNorm2d(16),
+            nn.Conv2d(12, 12, kernel_size=5, padding=2),
+            nn.BatchNorm2d(12),
             nn.ReLU(),
-            nn.MaxPool2d(2))
+            nn.MaxPool2d(1))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
-            nn.BatchNorm2d(32),
+            nn.Conv2d(12, 12, kernel_size=5, padding=2),
+            nn.BatchNorm2d(12),
             nn.ReLU(),
-            nn.MaxPool2d(2))
-        self.fc = nn.Linear(32, 10)
+            nn.MaxPool2d(1))
+        self.fc = nn.Linear(12, 10)
         
     def forward(self, x):
         out = self.layer1(x)
@@ -78,6 +78,7 @@ for epoch in range(num_epochs):
 #        # Forward + Backward + Optimize
 #        optimizer.zero_grad()
         outputs = cnn(images)
+        """The out put must be convert into Tk to be compare with labels"""
 #        
 #        
 #        
